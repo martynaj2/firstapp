b153932b1ec823836a252dd801e57077c50c5c5c
martynaj2 <martyna.jasiak@gmail.com>
2018-07-10 14:26:08 +0200

    [add] validation to post

--- app/models/post.rb
+++ app/models/post.rb
@@ -4,5 +4,11 @@ class Post < ActiveRecord::Base
   validates :title, length: { maximum: 80 }
   validates :content, length: { in: 10..500 }
 
+  scope :old, -> {where('created_at < ?', 40.minutes.ago)}
 
+  before_create :annotate_autor
+
+  def annotate_autor
+    self.autor = "#{self.autor} from Binarapps"
+  end
 end
