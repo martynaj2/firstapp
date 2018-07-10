b92b7cea5f828bde188b43f7f3a800a247b8d83c
martynaj2 <martyna.jasiak@gmail.com>
2018-07-10 15:33:47 +0200

    [add] validation to author

--- app/models/autor.rb
+++ app/models/autor.rb
@@ -1,4 +1,8 @@
 class Autor < ActiveRecord::Base
+  validates :name, :surname, presence: true
+  validates :name, length: { minimum: 2 }
+  validates :surname, length: { minimum: 5}
+
   has_many :autor_posts
   has_many :posts, through: :autor_posts
 end
