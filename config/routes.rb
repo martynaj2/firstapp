238979384240e54fc91101343e47bb32d9f21c21
martynaj2 <martyna.jasiak@gmail.com>
2018-07-11 13:07:05 +0200

    [add][update] routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
     root 'home#welcome'
     #metoda http, potem sciezka (strukutra po /), gdzie rootuje,
-    get '/autors', to: 'autors#index', as: :autors
-    get '/autors/:id', to: 'autors#show', as: :autor
-    get '/autor/new', to: 'autors#new', as: :new_autor
+    # get '/autors', to: 'autors#index', as: :autors
+    # get '/autors/:id', to: 'autors#show', as: :autor
+    # get '/autor/new', to: 'autors#new', as: :new_autor
+    resources :autors
 end
