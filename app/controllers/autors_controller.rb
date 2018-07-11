2771dc521e01ecbdad4dcb4428bec30b68aa7dc6
martynaj2 <martyna.jasiak@gmail.com>
2018-07-11 11:52:56 +0200

    [add][update]show.html.erb, routes.rb

--- app/controllers/autors_controller.rb
+++ app/controllers/autors_controller.rb
@@ -3,4 +3,9 @@ class AutorsController < ApplicationController
     #zmienna lokalna, nie jest widoczna w widoku, wiec robimy zmienna instacji
     @autors = Autor.all
   end
+
+  def show
+    #chcemy znalezc konkretnego autora po id, z URL, /autors/xx
+    @autor = Autor.find(params[:id])
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
     root 'home#welcome'
     #metoda http, potem sciezka (strukutra po /), gdzie rootuje,
     get '/autors', to: 'autors#index'
+    get '/autors/:id', to: 'autors#show'
 end
